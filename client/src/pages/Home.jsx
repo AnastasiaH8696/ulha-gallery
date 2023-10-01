46d76a1a97472ede0dfaacb21e1c7f242c4bf767
Anastasia Hamandritov <nasthg96@gmail.com>
2023-10-01 22:10:32 +0300

    adding dummy data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const images = [
+    { id: 1, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/1.jpeg" },
+    { id: 2, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/2.jpeg" },
+    { id: 3, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/3.jpeg" },
+    { id: 4, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/4.jpeg" },
+    { id: 5, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/5.jpeg" },
+    { id: 6, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/6.jpeg" },
+    { id: 7, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/7.jpeg" },
+    { id: 8, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/8.jpeg" },
+    { id: 9, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/9.jpeg" },
+    { id: 10, src: "https://github.com/AnastasiaH8696/ulha-gallery/blob/main/client/temp_resources/10.jpeg" },
+  ];
+
+  return (
+    <div>
+      <h1>Gallery</h1>
+      <div className="gallery">
+        {images.map((image) => (
+          <img key={image.id} src={image.src} alt={`Image ${image.id}`} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
